08e5a0452b4ccd55250d7113d159463a7d706f0f
Cosmas Aruba <cosmasarubaken@gmail.com>
2021-01-19 13:27:16 +0300

    added gatsby-gdpr-cookie-consent plugin

--- packages/personal-blog-lite/gatsby-config.js
+++ packages/personal-blog-lite/gatsby-config.js
@@ -126,6 +126,19 @@ module.exports = {
       
       },
     },
+ {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: 'UA-187038182-1',
+          // Setting this parameter is optional
+          anonymize: true
+        },
+       
+        // Defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development']
+      },
+    },
     {
       resolve: `gatsby-plugin-lodash`,
     }
